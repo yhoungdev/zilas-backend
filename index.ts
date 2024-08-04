e79233525fe59ea1bfcf1901c8b2f117951b60c0
yhoungdev <yhoungdev@gmail.com>
2024-08-04 15:17:35 +0100

    chore: updating index route ✋✋

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "server for zilas project" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
