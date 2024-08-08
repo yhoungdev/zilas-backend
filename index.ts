8b185bf6f4c83bd9fe105d7a80a5824d20a3c373
yhoungdev <yhoungdev@gmail.com>
2024-08-08 12:42:48 +0100

    up[date

--- index.ts
+++ index.ts
@@ -5,7 +5,19 @@ import { ROUTES_CONSTANT } from "./src/constant/routeConstant";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Authorization",
+  ],
+};
+
+app.use(cors(corsConfig));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "server for zilas project" });
