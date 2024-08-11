52191fe3771be2d219a6449e61d0eee6c6f49b0e
yhoungdev <yhoungdev@gmail.com>
2024-08-12 00:38:10 +0100

    fetching all products to users

---
+++ src/controllers/user/products.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express";
+import { prismaInstance } from "../../../utils/prisma";
+import { StatusCode } from "../../enums/statusEnum";
+export const fetchAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await prismaInstance.products.findMany();
+    return res.status(StatusCode.OK).json({
+      message: "Products fetched successfully",
+      data: products,
+    });
+  } catch (err) {
+    return res.status(StatusCode.InternalServerError).json({
+      //@ts-ignore
+      message: err?.message,
+      error: "Internal Server Error",
+    });
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,14 @@ import {
   getUserWallet,
 } from "../controllers/user/wallet";
 import { isAuthenticated } from "../../utils/isAuthenticated";
+import { fetchAllProducts } from "../controllers/user/products";
 
 const router = express.Router();
 
 router.get("/user/profile/:id", isAuthenticated, getUserprofileController);
 router.post("/user/wallet/add", isAuthenticated, addExternalWalletController);
 router.get("/user/wallet/", isAuthenticated, getUserWallet);
+router.get("/user/products", isAuthenticated, fetchAllProducts);
+
 
 export const defaultUsersRoute = router;
